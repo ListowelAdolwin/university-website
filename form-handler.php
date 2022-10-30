ce0f4ae9a937c3243e4aae1761f23234591043db
ListowelAdolwin <listoweladolwin@gmail.com>
2022-10-30 10:17:55 +0000

    Upgraded the form handler

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email']
 $subject = $_POST['subject']
 $message = $_POST['message']
 
-$email_from = 'listowelmoro0@gmail.com'
+$email_from = 'info@https://listoweladolwin.github.io/university-website'
 
 $email_subject = 'New Form Submission'
 
